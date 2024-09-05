9f750add38e3f10d04fd2a6813aa054864a0cd46
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-05 18:52:27 +0600

    Function JS File Done and Dusted

--- function.js
+++ function.js
@@ -1,3 +1,28 @@
 const price = 45;
 
 // Function Declare
+
+/**
+ * function name() {
+ *   -------------;
+ * }
+ */
+
+function fanOffKor() {
+    console.log("Stand up");
+    console.log("Walk towards to the switch");
+    console.log("Click the switch up to off the fan");
+}
+
+function brushTeeth() {
+    console.log("Pick up the brush");
+    console.log("Add paste to your brush");
+    console.log("Brush your teeth properly");
+    console.log("Clean up your face with water");
+}
+
+// Call the Function
+
+fanOffKor();
+
+brushTeeth();
